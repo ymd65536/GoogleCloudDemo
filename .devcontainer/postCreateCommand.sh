4052fa7bce36e4c1f5afa609a63de7db6019154f
yamada-kento <ke-yamada@cloudpack.jp>
2026-04-10 07:31:08 +0000

    feat: postCreateCommand.sh に gcloud SDK の初期設定を追加

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,7 @@
 # bin/bash
 curl -sSL https://sdk.cloud.google.com | bash -s -- --disable-prompts
+echo 'source "$HOME/google-cloud-sdk/path.zsh.inc"' >> ~/.zshrc
+echo 'source "$HOME/google-cloud-sdk/completion.zsh.inc"' >> ~/.zshrc
+echo 'source "$HOME/google-cloud-sdk/path.bash.inc"' >> ~/.bashrc
+echo 'source "$HOME/google-cloud-sdk/completion.bash.inc"' >> ~/.bashrc
 pip install --user google-api-python-client google-auth
