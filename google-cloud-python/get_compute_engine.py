9e48c4ddb0496716e2bf335a4afde5aa066e31ca
ymd65536 <ymd65536@gmail.com>
2026-04-14 15:31:04 +0900

    feat: Compute Engine のインスタンス状態を取得するサンプルを追加

---
+++ google-cloud-python/get_compute_engine.py
@@ -0,0 +1,12 @@
+"""
+google-cloud-python (google-cloud-compute) を使って
+Compute Engine のインスタンス状態を取得するサンプル
+
+インストール:
+    pip install google-cloud-compute
+
+使い方:
+    export GOOGLE_CLOUD_PROJECT=your-project-id
+    export GOOGLE_CLOUD_ZONE=us-central1-a  # 省略時は us-central1-a
+    python compute_engine.py
+"""
